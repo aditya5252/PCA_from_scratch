98faef93c14c6375cc3422ff6f6253adddf7e382
aditya5252 <aaditya.sharma1989@yahoo.com>
2022-02-16 23:53:36 +0530

    Pythonicmakingcode

--- src/data_read.py
+++ src/data_read.py
@@ -1,9 +1,12 @@
 '''Variable nomenclature improve'''
 import glob
 import matplotlib.pyplot as plt
-data = [(plt.imread(file),file) for file in glob.glob('..\data\*.gif')]
-img_list=[]
-img_targets=[]
-for dat in data :
-    img_list.append(dat[0])    
-    img_targets.append(dat[1])
+
+def get_data_():
+    data = [(plt.imread(file),file) for file in glob.glob('..\data\*.gif')]
+    img_list=[]
+    img_targets=[]
+    for dat in data :
+        img_list.append(dat[0])    
+        img_targets.append(dat[1])
+    return img_list,img_targets
